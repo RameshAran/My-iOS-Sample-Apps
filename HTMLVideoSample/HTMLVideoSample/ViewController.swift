e4ea7799e0cfb1d09b91e96ec58e3173e5ebc78e
ramesh_aran <rameshchandran86@gmail.com>
2017-03-16 00:46:45 +0530

    WKWebView with local HTML Video
    This is a sample app for displaying local HTML file in WKWebView with
    Video

---
+++ HTMLVideoSample/HTMLVideoSample/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  HTMLVideoSample
+//
+//  Created by Ramesh Chandran A on 15/03/17.
+//  Copyright © 2017 smartapps. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let path = Bundle.main.path(forResource: "Video_Sample", ofType: "html") {
+            webView.load(URLRequest(url : URL(fileURLWithPath: path)))
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
